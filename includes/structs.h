d32afd8c74e075fe40c7a64ed3e4330e3fbd6060
Emma Hedemann <ehedeman@c2r2s6.42wolfsburg.de>
2024-10-11 15:39:25 +0200

    memory for coordinates array needs to be allocated still, rest should be somewhat fine

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: ehedeman <ehedeman@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 13:32:40 by ehedeman          #+#    #+#             */
-/*   Updated: 2024/10/11 14:44:29 by ehedeman         ###   ########.fr       */
+/*   Updated: 2024/10/11 15:38:51 by ehedeman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ typedef struct s_coordinates
 
 typedef struct s_map
 {
-	char			*content;
+	char			*content;	//content of the map file
 	t_coordinates	top_l;
 	t_coordinates	top_r;
 	t_coordinates	bottom_l;
 	t_coordinates	bottom_r;
-	t_coordinates	point_zero;	// = bottom_r makes most sense
+	t_coordinates	point_zero;	// = top_l makes most sense due to the way the info is read
 	t_coordinates	**coords;	//array of coordinates
 }			t_map;
 
