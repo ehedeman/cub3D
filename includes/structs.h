291320edcc742fb2e12cad83828c57c566fb6901
Emma Hedemann <ehedeman@c2r2s6.42wolfsburg.de>
2024-10-18 12:05:18 +0200

    normed most things (where it made sense)

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: ehedeman <ehedeman@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 13:32:40 by ehedeman          #+#    #+#             */
-/*   Updated: 2024/10/18 12:00:13 by ehedeman         ###   ########.fr       */
+/*   Updated: 2024/10/18 12:04:53 by ehedeman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,26 +46,29 @@ typedef enum e_orientation
 which is why i have two seperate variables cuz i was tired of calculating shit
 */
 
+// x/y -> just for printing
+// x_array/y_array for changing locations etc
+// t_coordinates dont need to be freed anywhere but in map.coords
 typedef struct s_coordinates
 {
-	int	x;				//just for printing
-	int x_array;		//for changing locations etc
+	int	x;
+	int	x_array;
 	int	y;
 	int	y_array;
 	int	type;
 }			t_coordinates;
 
+// start->orientation = N, S, W, E, might not need it
 typedef struct s_player
 {
 	t_coordinates	*current;
 	t_coordinates	*next;
-	char			start_orientation;	//N,S,W,E, might not need it
+	char			start_orientation;
 	int				current_orientation;
 	void			*mlx;
 }				t_player;
-//t_coordinates dont need to be freed anywhere but in map.coords
 
-typedef struct	s_rgb
+typedef struct s_rgb
 {
 	int	r;
 	int	g;
@@ -82,27 +85,35 @@ typedef struct s_wall
 	t_rgb	ceiling;
 }				t_wall;
 
+// content = contents of the map file
+// allocated_rows = amount of rows in the coords array
+// -> (incl negative space and NULL row at end)
+// map_length = length of longest row
+// point_zero = bottom_l so it works like a normal coordinate system
+// coords = array of coordinates
 typedef struct s_map
 {
-	char			*content;	//content of the map file
-	int				allocated_rows;	//amount of rows in the coords array (incl negative space and NULL row at end)
-	int				map_length;	//length of longest row
+	char			*content;
+	int				allocated_rows;
+	int				map_length;
 	t_coordinates	*top_l;
 	t_coordinates	*top_r;
 	t_coordinates	*bottom_l;
 	t_coordinates	*bottom_r;
-	t_coordinates	*point_zero;	//bottom_l so it works like a normal coordinate system
+	t_coordinates	*point_zero;
 	t_coordinates	*player_start;
-	t_coordinates	**coords;	//array of coordinates
+	t_coordinates	**coords;
 }			t_map;
 
+// win_width set to 3000 by default at the moment
+// win_height set to 2000
 typedef struct s_mlx
 {
 	void		*mlx;
 	void		*mlx_window;
 	void		*test;
-	int			win_width;	//set to 3000 by default at the moment
-	int			win_height;	//set to 2000
+	int			win_width;
+	int			win_height;
 }				t_mlx;
 
 typedef struct s_game
